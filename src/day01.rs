39e89196dc67ed4d8b43e8377d79b9d7317e7ab4
Daniel Luoma <luomadaniel95@gmail.com>
2024-06-11 15:08:37 +0300

    day1 part 1 solution

--- src/day01.rs
+++ src/day01.rs
@@ -1,14 +1,38 @@
 use crate::utils::read_lines;
 
-pub fn run() {
+pub fn run_part1() {
     println!("Running day 01 solution.");
 
     match read_lines("input/day01.txt") {
         Ok(lines) => {
+            let mut calibration_sum = 0;
             for line in lines {
-                println!("{}", line);
+                calibration_sum += add_calibration_number(&line)
             }
+            println!("Calibration sum: {}", calibration_sum);
         }
         Err(e) => println!("Error: {}", e),
+    }
+}
+
+pub fn run_part2() {
+    println!("Hello main");
+}
+
+fn add_calibration_number(line: &str) -> i32 {
+    let first_digit = line.chars().find(|c| c.is_digit(10));
+    let last_digit = line.chars().rev().find(|c| c.is_digit(10));
+
+    if let (Some(first_digit), Some(last_digit)) = (first_digit, last_digit) {
+        let first_digit_str = first_digit.to_string();
+        let last_digit_str = last_digit.to_string();
+
+        let concat_str = first_digit_str + &last_digit_str;
+        let number: i32 = concat_str.parse().expect("Failed to parse concatenated number");
+        return number;
+    }
+    else {
+        println!("No digits found.");
+        return 0;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,18 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: cargo run -- <day>");
+    if args.len() < 3 {
+        println!("Usage: cargo run -- <day> <part>");
         return;
     }
 
     let day = &args[1];
+    let part = &args[2];
 
-    match day.as_str() {
-        "1" => day01::run(),
+    match (day.as_str(), part.as_str()) {
+        ("1", "1") => day01::run_part1(),
+        ("1", "2") => day01::run_part2(),
         // Keep adding days
-        _ => println!("Invalid day: {}", day),
+        _ => println!("Invalid day or part: {} {}", day, part),
     }
 }
