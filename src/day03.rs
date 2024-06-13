41f1b690df81cd3955a581aa75289dcc53d08757
Daniel Luoma <luomadaniel95@gmail.com>
2024-06-13 15:19:34 +0300

    [day 03 part 1] add adjacent number recognition

--- src/day03.rs
+++ src/day03.rs
@@ -12,6 +12,30 @@ fn is_symbol(c: char) -> bool {
     (c.is_ascii_punctuation() || c.is_ascii_graphic() && !c.is_alphanumeric()) && c != '.'
 }
 
+fn collect_adjacent_numbers(grid: &Vec<Vec<char>>, row: usize, col: usize) {
+    let directions = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, -1),
+        (-1, 1),
+        (1, -1),
+        (1, 1),
+    ];
+
+    for &(d_row, d_col) in &directions {
+        let new_row = row.wrapping_add(d_row as usize);
+        let new_col = col.wrapping_add(d_col as usize);
+
+        if new_row < grid.len() && new_col < grid[new_row].len() {
+            if grid[new_row][new_col].is_numeric() {
+                println!("{} is adjacent to {}", grid[new_row][new_col], grid[row][col]);
+        }
+        }
+    }
+}
+
 pub fn run_part1() {
     println!("Running day 03 part 1 solution.");
 
@@ -25,14 +49,14 @@ pub fn run_part1() {
 
         }
         Err(e) => println!("Error: {}", e),
-        
     }
-
 
     for row_index in 0..grid.len() {
         for col_index in 0..grid[row_index].len() {
             let c = grid[row_index][col_index];
-            println!("Is '{}' a symbol? {}", c, is_symbol(c))
+            if is_symbol(c) {
+                collect_adjacent_numbers(&grid, row_index, col_index)
+            }
         }
         println!();
     }
