3b1614a270eeb6dd8c6edd989443563a445a3a5b
Daniel Luoma <luomadaniel95@gmail.com>
2024-06-26 11:10:24 +0300

    [day05 part 1] init.

---
+++ src/day05.rs
@@ -0,0 +1,5 @@
+use crate::utils::read_lines;
+
+pub fn run_part1() {
+    println!("Running day 05 part 1 solution.");
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 use std::env;
 
@@ -23,6 +24,7 @@ fn main() {
         ("2", "2") => day02::run_part2(),
         ("3", "1") => day03::run_part1(),
         ("4", "1") => day04::run_part1(),
+        ("5", "1") => day05::run_part1(),
         // Keep adding days
         _ => println!("Invalid day or part: {} {}", day, part),
     }
