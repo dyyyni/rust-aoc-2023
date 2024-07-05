258703a47b0c3732e37b0644dd685d855bf96dfb
Daniel Luoma <luomadaniel95@gmail.com>
2024-07-05 16:43:05 +0300

    [day07 part 1] add data structures

--- src/day07.rs
+++ src/day07.rs
@@ -1,4 +1,33 @@
+use crate::utils::read_lines;
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+enum Cards {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+struct Hand {
+    cards: Vec<Cards>, 
+    bid: u64,
+}
+
+impl Hand {
+    fn new(cards: Vec<Cards>, bid: u64) -> Hand {
+        Hand { cards, bid }
+    }
+    
+}
 
 pub fn run_part1() {
     println!("Running day07 part 1 solution.");
